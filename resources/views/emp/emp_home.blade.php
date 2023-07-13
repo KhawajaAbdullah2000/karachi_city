c0bf6eb9163bea25207f5163c201e3f7258c84ee
Shahzaib <mshahzaib2110@gmail.com>
2023-07-13 08:50:35 -0700

    added emp-nav blade made changes to Branch Controller to assign manager

--- resources/views/emp/emp_home.blade.php
+++ resources/views/emp/emp_home.blade.php
@@ -1,8 +1,8 @@
 @extends('master')
 @section('content')
-
-<h1>Welcome Employee {{auth()->user()->name}}</h1>
+@include('emp-nav')
+<h1>Welcome {{auth()->user()->name}}</h1>
 
 <br>
-<a href="{{ route('logout') }}" class="btn btn-primary">Log out</a>
+
 @endsection
