f5337a51deae96f92266f6c3c404166def4a85c4
unknown <k200385@nu.edu.pk>
2023-07-14 11:05:35 +0500

    added student homepage navbar

--- resources/views/student/student_home.blade.php
+++ resources/views/student/student_home.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
 
+@include('student.student_nav')
+
+<div class="container">
+
 
     
 <h1>Welcome Student  {{auth('student')->user()->first_name}}</h1>
 
 <a href="{{ url('/student_logout') }}" class="btn btn-primary">Log out</a>
 
-<br>
 
 
 </div> 
 
-</div> 
 
 
 @endsection
