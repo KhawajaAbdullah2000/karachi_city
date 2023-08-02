c21213181a3a5e3a2982109da4d9582a87fbab8a
unknown <k200385@nu.edu.pk>
2023-08-02 17:46:35 +0500

    changed mail password

--- resources/views/student/admission_invoice.blade.php
+++ resources/views/student/admission_invoice.blade.php
@@ -1,6 +1,10 @@
+@extends('master')
+
+@section('content')
+
 <div class="invoice-container">
     <div class="invoice-header">
-        <h2 class="invoice-title">Welcome {{$f_name}} {{$l_name}} To karachi city</h2>
+        <h2 class="invoice-title" style="background-color: blue">Welcome {{$f_name}} {{$l_name}} To karachi city</h2>
     </div>
     <h3>You can join us after you pay. Bring the receipt to the branch to complete your registeration</h3>
     <div class="invoice-details">
@@ -20,4 +24,6 @@
         <p>Total: 6000 RS</p>
     </div>
 </div>
-</body>
+
+
+@endsection
